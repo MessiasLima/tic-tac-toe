2974bdc4d30f95814ce056a260150dee045a7bda
Messias Lima <messiaslima.03@gmail.com>
2018-09-12 12:43:01 -0300

    fix: clear

--- src/main/java/br/com/cajumobile/tictactoe/CommandLine.java
+++ src/main/java/br/com/cajumobile/tictactoe/CommandLine.java
@@ -1,34 +1,20 @@
 package br.com.cajumobile.tictactoe;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 class CommandLine {
 
     private final Scanner scanner;
-    private OS system;
 
-    public CommandLine() {
-        system = getOS();
+    CommandLine() {
         scanner = new Scanner(System.in);
     }
 
-    private OS getOS() {
-        String osName = System.getProperty("os.name").toLowerCase();
-        if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) {
-            return OS.LINUX;
-        } else if (osName.contains("win")) {
-            return OS.WINDOWS;
-        } else {
-           return OS.UNKNOWN;
-        }
-    }
-
-    public void write(String text) {
+    void write(String text) {
         write(text, true);
     }
 
-    public void write(String text, boolean newLine) {
+    void write(String text, boolean newLine) {
         if (newLine) {
             System.out.println(text);
         } else {
@@ -36,24 +22,7 @@ class CommandLine {
         }
     }
 
-    public void clear() {
-        try {
-            switch (system) {
-                case LINUX:
-                    Runtime.getRuntime().exec("clear");
-                    break;
-                case WINDOWS:
-                    Runtime.getRuntime().exec("cls");
-                    break;
-                default:
-                    clearByNewLine();
-            }
-        } catch (IOException ioe) {
-            clearByNewLine();
-        }
-    }
-
-    private void clearByNewLine() {
+    void clear() {
         write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
@@ -61,7 +30,7 @@ class CommandLine {
         write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
 
-    public String read() {
+    String read() {
         return scanner.next();
     }
 }
